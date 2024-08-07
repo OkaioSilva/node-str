51fa0bcd5cd08dc213f73d772ef9f53788e49ad2
Kaio <silv4kaio@hotmail.com>
2024-08-07 14:22:28 -0300

    rotas

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
+'use strict'
 //1 - importar o express
 const express = require('express')
 
 //2 - crie a aplicação:
 const app = express()
 const router = express.Router()
+
+// 6 - body parser express
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 //3 - criando a primeira rota
 const route = router.get('/', (req, res, next)=>{
@@ -12,7 +17,29 @@ const route = router.get('/', (req, res, next)=>{
             version: "0.0.1"
         })
     })
-    app.use('/', route)
+    
+//5 - novas rotas
+    // create
+    const create = router.post('/', (req, res, next)=>{
+        res.status(201).send(req.body)
+    })
+    // put 
+    const put = router.put('/:id', (req, res, next)=>{
+        const id = req.params.id;
+        res.status(201).send({
+            id: id, 
+            item: req.body
+        })
+    })
 
-// exportando app
+    // delete
+    const del = router.delete('/', (req, res, next)=>{
+        res.status(200).send(req.body)
+    })
+    
+    app.use('/', route)
+    app.use('/products', create);
+    app.use('/products', put);
+    app.use('/products', del);
+//4 - exportando app
 module.exports = app
