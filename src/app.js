7d02f74d542a598b75d068b18d0bbe1220b8e747
Kaio <silv4kaio@hotmail.com>
2024-09-03 23:37:37 -0300

    Corrigindo o res.headers, no methods

--- src/app.js
+++ src/app.js
@@ -80,9 +80,9 @@ app.use(express.json())
 
     // habilita o CORS
     app.use(function(req, res, next){
-        res.header('Access-Control-Allow-Origiin', '*');
+        res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
-        res.header('Access-Control-Allow', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         next();
     })
 
